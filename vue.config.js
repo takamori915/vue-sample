7a90b982e82ad8c63c95569f6c9172843a239569
takamori <takamori915@gmail.com>
2022-08-17 09:46:47 +0900

    vue.config.jsを変更（netlify公開用に）

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   transpileDependencies: [],
-  publicPath: isProd ? '/vue-sample/' : '',
+  publicPath: isProd ? '' : '',
   outputDir: 'dist',
   filenameHashing: false,
   productionSourceMap: false
